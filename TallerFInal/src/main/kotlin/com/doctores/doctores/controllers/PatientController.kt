0db4a0f9daa43152ba5be16338b2812279b58295
Eriik12 <lamprearivero@gmail.com>
2023-07-22 23:52:43 -0500

    CRUD COMPLETO, falta act upd

---
+++ TallerFInal/src/main/kotlin/com/doctores/doctores/controllers/PatientController.kt
@@ -0,0 +1,42 @@
+package com.doctores.doctores.controllers
+import com.doctores.doctores.constants.*
+import com.doctores.doctores.domains.entity.Patient
+import com.doctores.doctores.domains.request.CreateDoctorRequest
+import com.doctores.doctores.domains.request.PatientRequest
+import com.doctores.doctores.domains.responses.PatientResponse
+import com.doctores.doctores.services.PatientService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class PatientController {
+    @Autowired
+    private lateinit var patientService: PatientService
+
+    @GetMapping(Patient)
+    fun getAllPatients(): List<Patient> = patientService.getAllPatients()
+
+    @PostMapping(CreatePatients)
+    fun createPatient(
+            @RequestBody @Validated request: PatientRequest
+    ): PatientResponse = patientService.createPatient(request)
+
+    @GetMapping(GetPatientById)
+    fun getPatienttById(
+            @PathVariable("id") id: Long
+    ): List<Patient> = patientService.getPatientById(id)
+
+    @PutMapping("/{id}/update")
+    fun updatePatient(
+        @PathVariable("id") id: Long,
+        @RequestBody @Validated request: PatientRequest
+    ): String  = patientService.updatePatient(id, request)
+
+    @DeleteMapping(DeletePatient)
+    fun deletePatient(
+            @PathVariable("id")id:Long
+    ): Unit = patientService.deletePatientById(id)
+
+
+}
